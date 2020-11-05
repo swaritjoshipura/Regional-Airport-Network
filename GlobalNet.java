04f5f27d1d8cf662811ae25a277326a4f08f78c7
Swarit Joshipura <54641768+swaritjoshipura@users.noreply.github.com>
2020-11-05 11:29:35 -0800

    Create GlobalNet.java

---
+++ GlobalNet.java
@@ -0,0 +1,11 @@
+public class GlobalNet {
+    //creates a global network 
+    //O : the original graph
+    //regions: the regional graphs
+    public static Graph run(Graph O, Graph[] regions) {
+        //To be implemented
+        return null;
+    }
+}
+    
+    
